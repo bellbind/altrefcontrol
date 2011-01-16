f45708f8b07e6c016a2b00114d16dde6f1478fed
bellbind <bellbind@gmail.com>
2011-01-17 00:40:45 +0900

    [fix]

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-XPI=$1
+VERSION=$1
+XPI=altrefcontrol-$VERSION.xpi
 
 find . -print | grep -v '^\./\.git' | grep -v '\.xpi$' | grep -v 'update.rdf$' | zip -@ $XPI
 SHA1=`sha1sum $XPI | awk '{print $1}'`
 
-sed -e "s/@@XPI@@/$XPI/g;s/@@SHA1@@/$SHA1/g" update.rdf.xml > update.rdf
-
-
+sed -e "s/@@VERSION@@/$VERSION/g;s/@@XPI@@/$XPI/g;s/@@SHA1@@/$SHA1/g" update.rdf.xml > update.rdf
