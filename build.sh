b49497ba40dd6ba8cf6e1fb955fcb93fa3e28c70
bellbind <bellbind@gmail.com>
2011-01-17 00:27:51 +0900

    [fix]

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 XPI=$1
 
-find . -name '*' -print | grep -v '\.xpi' | zip -@ $XPI
+find . -print | grep -v '^\./\.git' | grep -v '\.xpi$' | grep -v 'update.rdf$' | zip -@ $XPI
 SHA1=`sha1sum $XPI | awk '{print $1}'`
 
 sed -e "s/@@XPI@@/$XPI/g;s/@@SHA1@@/$SHA1/g" update.rdf.xml > update.rdf
