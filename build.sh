8e278af706e92fd3345fe4ead9a7e4e000746dab
bellbind <bellbind@gmail.com>
2011-01-17 20:49:05 +0900

    [update] build.sh for use ruby instead of awk

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# automated tool for building xpi and update.rdf
+# required commands: ruby find grep zip sha1sum sed
 
 VERSION=`ruby -ane  'print $1 if /<em:version>(.*)<\/em:version>/' install.rdf`
 XPI=altrefcontrol-$VERSION.xpi
@@ -7,6 +9,6 @@ find . -print | \
   grep -v '^\./\.git' | grep -v '^\./profile/' | \
   grep -v '\.xpi$' | grep -v '\.sh$' | grep -v 'update.rdf$' | \
   zip -@ $XPI
-SHA1=`sha1sum $XPI | awk '{print $1}'`
+SHA1=`sha1sum $XPI | ruby -ane 'print $F[0]'`
 
 sed -e "s/@@VERSION@@/$VERSION/g;s/@@XPI@@/$XPI/g;s/@@SHA1@@/$SHA1/g" update.rdf.xml > update.rdf
